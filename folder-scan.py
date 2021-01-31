cf2a6ba5185096a49092c8bcc12a9d2b17c9378a
monofuel <monofuel@japura.net>
2021-01-30 16:17:54 -0800

    progress

--- folder-scan.py
+++ folder-scan.py
@@ -1,2 +1,85 @@
 #!/usr/bin/python3
 import os
+import subprocess
+import hashlib
+import json
+
+# script assumes running as root
+
+wallet_dict = {}
+
+# NB. this script indexes wallets based on md5sum. if multiple wallets share the same sum, only one is tracked
+# TODO should track multiple dirs
+
+# NB. I have no clue what the wallet format is, but they generally seem to start with this
+# btc wallets have '0000 0010' after this, dogecoin has '0000 0020'
+likely_header = bytearray.fromhex("000000000100000000000000623105000900")
+
+class Wallet:
+    dir = ""
+    def __init__(self, dir):
+        self.dir = dir
+
+    def contents(self):
+        f = open('./mounts/{}'.format(self.dir), mode='rb')
+        contents = f.read()
+        f.close()
+        return contents
+
+    def likelyWallet(self):
+        contents = self.contents()
+        return contents.startswith(likely_header)
+
+    def md5hash(self):
+        return hashlib.md5(self.contents()).hexdigest()
+
+def main():
+    find_output = subprocess.getoutput('cd ./mounts && find . -type f -iname "*wallet*"')
+    lines = find_output.splitlines()
+    for l in lines:
+        wallet = Wallet(l)
+        wallet_dict[wallet.md5hash()] = wallet
+
+    print()
+    wallet_meta  = {}
+    try:
+        meta_file = open("./wallets/meta.json", 'r')
+        contents = meta_file.read()
+        meta_file.close()
+        wallet_meta = json.loads(contents)
+        print('loaded existing ./wallets/meta.json')
+    except:
+        print("failed to load wallets/meta.json")
+        wallet_meta = {}
+        
+    try:
+        os.mkdir("./wallets")
+    except FileExistsError:
+        print('./wallets exists')
+    for wallet in wallet_dict.values():
+        md5hash = wallet.md5hash()
+        wallet_name = "./wallets/{}.wallet.dat".format(md5hash)
+        f_wallet = open(wallet_name, 'wb')
+        f_wallet.write(wallet.contents())
+        f_wallet.close()
+        # print("wrote out " + wallet_name)
+
+        meta = {
+            'hash': md5hash,
+            'fulldir': wallet.dir,
+            'drive': os.path.split(os.path.dirname(wallet.dir))[0],
+            'filename': os.path.basename(wallet.dir),
+            'likelyWallet': wallet.likelyWallet()
+        }
+        wallet_meta[md5hash] = meta
+        if meta["likelyWallet"]:
+            print("likely wallet: {} at {}".format(meta["hash"], meta["fulldir"]))
+
+    # handle metadata file for tracking wallets
+    wallet_meta_f = open('./wallets/meta.json', 'w')
+    wallet_meta_f.write(json.dumps(wallet_meta, indent=4, sort_keys=True))
+    wallet_meta_f.close()
+    print('wrote out ./wallets/meta.json')
+
+
+main()
--- mount-drives.py
+++ mount-drives.py
@@ -8,22 +8,31 @@ import subprocess
 ignore_drives = ("7a907b0c-4f8a-4350-b2b4-c804abca9622", "ed5b6085-40e0-463e-b2fa-438f6cb9ddcb", "6d406c55-5de8-4401-b2ee-92bd1046520c", "80b7f420-7699-476c-9db1-c0108bb661aa")
 def main():
     
-    check_disks = boot_files =  [f for f in os.listdir('/dev/disk/by-uuid') if f not in ignore_drives]
+    check_disks = [f for f in os.listdir('/dev/disk/by-uuid') if f not in ignore_drives]
     print("drives to mount: ", check_disks)
     try:
         os.mkdir("./mounts")
     except FileExistsError:
-        print('/mount exists')
+        print('./mount exists')
         
+    print("cleaning up mounts folder")
+    clean_mounts = os.listdir("./mounts")
+    for f in clean_mounts:
+        subprocess.run('umount ./mounts/{}'.format(f), shell=True)
+        subprocess.run("rmdir ./mounts/{}".format(f), shell=True)
+        print('unmounted ' + f)
+
     mounts = subprocess.getoutput('mount')
-    
+    print("mounting disks")    
     for f in check_disks:
         try:
             os.mkdir("./mounts/" + f)
         except FileExistsError:
             print('folder exists: ' + f)
+
         if not  'mounts/{}'.format(f) in mounts:
-            subprocess.run('mount /dev/disk/by-uuid/{} ./mounts/{}'.format(f, f), shell=True)
+            subprocess.run('mount -o ro /dev/disk/by-uuid/{} ./mounts/{}'.format(f, f), shell=True)
+            print('mounted: ' + f)
         else:
             print('folder already mounted: ' + f)
 
